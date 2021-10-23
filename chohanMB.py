ad949413154c835055fb6eca3b750bce131a1f8e
unknown <bridgham.ma@gmail.com>
2021-10-23 13:02:05 -0700

    none

---
+++ chohanMB.py
@@ -0,0 +1,102 @@
+# https://nostarch.com/big-book-small-python-projects
+# Cho-Han, by Al Sweigart al@inventwithpython.com
+# The traditional Japanese dice game of even-odd.
+
+import random, sys
+
+JAPANESE_NUMBERS = {1: 'ICHI', 2: 'NI', 3: 'SAN',
+                    4: 'SHI', 5: 'GO', 6: 'ROKU'}
+
+print('''Cho-han is an intrinsic part of Japanese culture, with the game
+stretching back centuries in its popularity. It was originally played by
+bakuto, who were nomadic gamblers who moved from town to town winning bets
+off the local people. They are considered forerunners of organised crime
+groups like the Yakuza, among whom Cho-han is still popular today.
+
+The rules of Cho-han quite simple. To play, a dealer shakes two dice inside
+a bamboo cup, then turns it over to conceal the dice within. At this point,
+players must place their bets on whether the total of the numbers on the
+upturned faces of the dice will be even (Cho) or odd (Han).
+
+In this program, you are in a Japanese gambling house. The game of choice is
+Cho-Han. The stakes are high. You have 5000 coins in your purse to start. See
+if you can win but be careful or the Yakuza may just think you are cheating and
+a money will be the least of your problems.
+''')
+
+purse = 5000
+while True:  # Main game loop.
+    
+    # Place your bet:
+    
+    print('You have', purse, 'mon. How much do you dare to bet? (or QUIT)')
+    while True:
+        pot = input('> ')
+        if pot.upper() == 'QUIT':
+            print('Hey! Come back! We weren\'t done playing yet!!')
+            sys.exit()
+        elif not pot.isdecimal():
+            print('Please enter a number.')
+        elif int(pot) > purse:
+            print('You do not have enough to make that bet.')
+        else:
+            
+            # This is a valid bet.
+            
+            pot = int(pot)  # Convert pot to an integer.
+            break  # Exit the loop once a valid bet is placed.
+
+    # Roll the dice.
+    
+    dice1 = random.randint(1, 6)
+    dice2 = random.randint(1, 6)
+
+    print('The dealer smiles as he swirls the cup and you hear the rattle of dice.')
+    print('The dealer slams the cup on the floor, still covering the')
+    print('dice and asks for your bet.')
+    print()
+    print('    CHO (even) or HAN (odd)?')
+
+    # Let the player bet cho or han:
+    
+    while True:
+        bet = input('> ').upper()
+        if bet != 'CHO' and bet != 'HAN':
+            print('Please enter either "CHO" or "HAN".')
+            continue
+        else:
+            break
+
+    # Reveal the dice results:
+    
+    print('Slowly the dealer lifts the cup to reveal:')
+    print('  ', JAPANESE_NUMBERS[dice1], '-', JAPANESE_NUMBERS[dice2])
+    print('    ', dice1, '-', dice2)
+
+    # Determine if the player won:
+    
+    rollIsEven = (dice1 + dice2) % 2 == 0
+    if rollIsEven:
+        correctBet = 'CHO'
+    else:
+        correctBet = 'HAN'
+
+    playerWon = bet == correctBet
+
+    # Display the bet results:
+    
+    if playerWon:
+        print('You won! The dealer eyes you suspiciously as you take', pot, 'mon.')
+        purse = purse + pot  # Add the pot from player's purse.
+        print('The dealer smiles as the house collects a', pot // 10, 'mon fee for it\'s troubles.')
+        purse = purse - (pot // 10)  # The house fee is 10%.
+    else:
+        purse = purse - pot  # Subtract the pot from player's purse.
+        print('The dealer laughs as he takes your money. Too bad. That\'s rotten luck! Dare to try again friend?')
+
+    # Check if the player has run out of money:
+    
+    if purse == 0:
+        print('Hmm. Looks like your well has run dry my friend. Come back again when you have more money.')
+        print('Better luck next time!')
+        sys.exit()
---
+++ fibonacciMB.py
@@ -0,0 +1,96 @@
+# https://nostarch.com/big-book-small-python-programming
+# Fibonacci Sequence
+# Calculates numbers of the Fibonacci sequence: 0, 1, 1, 2, 3, 5, 8, 13...
+
+import sys
+
+print('''Fibonacci was not the first to know about the sequence, it was known
+in India hundreds of years before! His real name was Leonardo Pisano Bogollo,
+and he lived between 1170 and 1250 in Italy. "Fibonacci" was his nickname, which
+roughly means "Son of Bonacci". As well as being famous for the Fibonacci Sequence,
+he helped spread Hindu-Arabi Numerals (like our present numbers 0, 1, 2, 3, 4, 5,
+6, 7, 8, 9) through Europe in place of Roman Numberals (I, II, III, IV, V, etc).
+Fibonacci Day is November 23rd, as it has the digits "1, 1, 2, 3" which is part
+of the sequence. The Fibonacci sequence begins with 0 and 1, and the next number
+is the sum of the previous two numbers. The sequence continues forever:
+
+0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987...
+
+To use this program, enter the length of the sequence you would like to generate
+below. The higher number you choose the more of the sequence will be generated and
+the longer it will take.\n''')
+
+while True:  # Main program loop.
+    
+    while True:  # Keep asking until the user enters valid input.
+        
+        print('Enter the Nth Fibonacci number you wish to')
+        print('calculate (such as 5, 50, 1000, 9999), or QUIT to quit:')
+        response = input('> ').upper()
+
+        if response == 'QUIT':
+            print('Thanks for playing!')
+            sys.exit()
+
+        if response.isdecimal() and int(response) != 0:
+            nth = int(response)
+            break  # Exit the loop when the user enteres a valid number.
+
+        print('Please enter a number greater than 0, or QUIT.')
+    print()
+
+    # Handle the special cases if the user entered 1 or 2:
+    
+    if nth == 1:
+        print('0')
+        print()
+        print('The #1 Fibonacci number is 0.')
+        continue
+    elif nth == 2:
+        print('0, 1')
+        print()
+        print('The #2 Fibonacci number is 1.')
+        continue
+
+    # Display warning if the user entered a large number:
+    
+    if nth >= 10000:
+        print('WARNING: This will take a while to display on the')
+        print('screen. If you want to quit this program before it is')
+        print('done, press Ctrl-C.')
+        input('Press Enter to begin...')
+
+    # Calculate the Nth Fibonacci number:
+    
+    secondToLast = 0
+    last = 1
+    NumCalculated = 2
+    print('0, 1, ', end='')  # Display the first two Fibonacci numbers.
+
+    # Display all the later numbers of the Fibonacci sequence:
+    
+    while True:
+        nextNum = secondToLast + last
+        NumCalculated += 1
+
+        # Display the next number in the sequence:
+        
+        print(nextNum, end='')
+
+        # Check if we've found the Nth number the user wants:
+        
+        if NumCalculated == nth:
+            print()
+            print()
+            print('The #', NumCalculated, ' Fibonacci ',
+                  'number is ', nextNum, sep='')
+            break
+
+        # Print a comma in between the sequence numbers:
+        
+        print(', ', end='')
+
+        # Shift the last two numbers:
+        
+        secondToLast = last
+        last = nextNum
